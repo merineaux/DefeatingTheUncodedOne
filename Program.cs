f732131e2a9ccc6f654661c44d8c04b7d9bbf400
merineaux <merineaux@proton.me>
2025-10-04 01:54:29 -0500

    started some of exercises for TCSPG | having fun

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Thing Namer 3000
+// changed variable names to make it better
+// also took out non-user input str variables
+// Console.WriteLine("What kind of thing are we talking about?");
+// string? thing = Console.ReadLine(); // noun
+// Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// string? desc = Console.ReadLine(); // adjective
+// Console.WriteLine("The " + desc + " " + thing + " of Doom 3000!");
+
+// The Variable Shop
+// string + char
+string str = "i am a string!";
+str = "changed!";
+Console.WriteLine(str);
+char c = 'a';
+c = 'w';
+Console.WriteLine(c);
+
+// numbers 
+int num = 0;
+num++;
+Console.WriteLine((num).ToString());
+uint num2 = 0;
+num2++;
+Console.WriteLine((num2).ToString());
+byte byteNum = 0;
+byteNum = 3;
+Console.WriteLine(byteNum);
+sbyte sbyteNum = 0;
+sbyteNum = 4;
+Console.WriteLine(sbyteNum);
+short shortNum = 0;
+shortNum = 5;
+Console.WriteLine((shortNum).ToString());
+ushort ushortNum = 0;
+ushortNum = 6;
+Console.WriteLine((ushortNum).ToString());
+long longNum = 0;
+longNum = 78;
+Console.WriteLine((longNum).ToString());
+ulong ulongNum = 0;
+ulongNum = 77;
+Console.WriteLine((ulongNum).ToString());
+
+// real numbers
+float floatNum = 0;
+floatNum = 1.2f;
+Console.WriteLine(floatNum);
+double doubleNum = 0;
+doubleNum = 1.2;
+Console.WriteLine(doubleNum);
+decimal decimalNum = 0;
+decimalNum = 1.2m;
+Console.WriteLine(decimalNum);
+
+// bool
+bool boolNum = false;
+boolNum = true;
+Console.WriteLine((boolNum).ToString());
+
+
+
+
+
+
+
+
+
+
+
+
+
